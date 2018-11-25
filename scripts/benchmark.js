91550c89c26811ed7f6325409ad76f5c262d3fb1
Ronen Amiel <ronen.amiel@gmail.com>
2018-11-25 11:48:00 +0200

    update .eslintrc

--- scripts/benchmark.js
+++ scripts/benchmark.js
@@ -1,15 +1,22 @@
+const { spawnSync } = require('child_process');
 
-const {spawnSync} = require('child_process');
+spawnSync('npm', ['install'], { stdio: 'inherit' });
 
-spawnSync('npm', ['install'], {stdio: 'inherit'});
+spawnSync('npx', ['bipbip', '-s', 'cache-benchmarks.json'], {
+  stdio: 'inherit',
+  encoding: 'utf-8',
+});
 
-spawnSync('npx', ['bipbip', '-s', 'cache-benchmarks.json'], {stdio: 'inherit', encoding: 'utf-8'});
+spawnSync('rm', ['-rf', 'node_modules', 'package-lock.json'], {
+  stdio: 'inherit',
+});
+spawnSync('git', ['clean', '-df'], { stdio: 'inherit' });
+spawnSync('git', ['checkout', '.'], { stdio: 'inherit' });
 
-spawnSync('rm', ['-rf', 'node_modules', 'package-lock.json'], {stdio: 'inherit'});
-spawnSync('git', ['clean', '-df'], {stdio: 'inherit'});
-spawnSync('git', ['checkout', '.'], {stdio: 'inherit'});
+spawnSync('git', ['checkout', 'origin/master'], { stdio: 'inherit' });
+spawnSync('npm', ['install'], { stdio: 'inherit' });
 
-spawnSync('git', ['checkout', 'origin/master'], {stdio: 'inherit'});
-spawnSync('npm', ['install'], {stdio: 'inherit'});
-
-spawnSync('npx', ['bipbip', '-c', 'cache-benchmarks.json'], {stdio: 'inherit', encoding: 'utf-8'});
+spawnSync('npx', ['bipbip', '-c', 'cache-benchmarks.json'], {
+  stdio: 'inherit',
+  encoding: 'utf-8',
+});
