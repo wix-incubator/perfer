23fe1489cafd4af02c0621a43835fc75df049241
Ran Yitzhaki <ranyitz@gmail.com>
2018-11-25 10:45:05 +0200

    compare

--- scripts/benchmark.js
+++ scripts/benchmark.js
@@ -3,7 +3,7 @@ const {spawnSync} = require('child_process');
 
 spawnSync('npm', ['install'], {stdio: 'inherit'});
 
-const {stdout: stdoutDiff} = spawnSync('npm', ['test'], {encoding: 'utf-8'});
+const {stdout: stdoutDiff} = spawnSync('npx', ['bipbip', '-s', '.cache/benchmarks.json'], {encoding: 'utf-8'});
 
 spawnSync('rm', ['-rf', 'node_modules', 'package-lock.json'], {stdio: 'inherit'});
 spawnSync('git', ['clean', '-df'], {stdio: 'inherit'});
@@ -12,7 +12,7 @@ spawnSync('git', ['checkout', '.'], {stdio: 'inherit'});
 spawnSync('git', ['checkout', 'origin/master'], {stdio: 'inherit'});
 spawnSync('npm', ['install'], {stdio: 'inherit'});
 
-const {stdout: stdoutBase} = spawnSync('npm', ['test'], {encoding: 'utf-8'});
+const {stdout: stdoutBase} = spawnSync('npx', ['bipbip', '-c', '.cache/benchmarks.json'], {encoding: 'utf-8'});
 
 console.log('stdoutDiff: ', stdoutDiff);
 console.log('stdoutBase: ', stdoutBase);
