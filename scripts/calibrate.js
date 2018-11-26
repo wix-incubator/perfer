d2ba9ce51d23dae126dd38d63788540093156df0
Ran Yitzhaki <ranyitz@gmail.com>
2018-11-26 11:16:24 +0200

    wip

--- scripts/calibrate.js
+++ scripts/calibrate.js
@@ -1,15 +1,55 @@
 const runBenchmarks = require('../src/runBenchmarks');
 const compareResults = require('../src/compareResults');
+const renderGraphs = require('../src/renderGraphs');
+const { Stats } = require('fast-stats');
 
-async function run() {
-  const result1 = await runBenchmarks(10000);
-  const result2 = await runBenchmarks(10000);
+// run two benchmarks and return the diff
+async function diffTwoBenchmarks(times = 1) {
+  const result1 = await runBenchmarks(times);
+  const result2 = await runBenchmarks(times);
 
-  console.log(await compareResults(result1, result2));
+  return await compareResults(result1, result2);
 }
 
-(async function() {
-  for (let i = 0; i < 1; i++) {
-    await run();
+async function runMultipleDiffs(diffRuns) {
+  const scenarioName = 'recursiveWithMemoization';
+  const medians = [];
+  const means = [];
+
+  for (let i = 0; i < diffRuns; i++) {
+    const diff = await diffTwoBenchmarks(10000);
+
+    const { median, mean } = diff.find(
+      el => el.scenarioName === scenarioName,
+    ).difference;
+
+    medians.push(median);
+    means.push(mean);
   }
-})();
+
+  const meansStats = new Stats().push(means);
+  meansStats.iqr();
+
+  const mediansStats = new Stats().push(medians);
+  mediansStats.iqr();
+
+  // console.log(medians, means);
+  const graphs = [
+    {
+      name: `median of ${scenarioName}`,
+      data: mediansStats.data.sort((a, b) => a > b),
+      mean: mediansStats.amean(),
+      median: mediansStats.median(),
+    },
+    {
+      name: `means of ${scenarioName}`,
+      data: meansStats.data.sort((a, b) => a > b),
+      mean: meansStats.amean(),
+      median: meansStats.median(),
+    },
+  ];
+
+  renderGraphs(graphs);
+}
+
+runMultipleDiffs(10);
--- scripts/showGraph.js
+++ scripts/showGraph.js
@@ -1,42 +1,50 @@
 const runBenchmarks = require('../src/runBenchmarks');
-const _ = require('lodash');
-const express = require('express');
-const openBrowser = require('react-dev-utils/openBrowser');
+const renderGraphs = require('../src/renderGraphs');
 
-function prepareDataForGraph(data) {
-  return _.chain(data)
-    .groupBy()
-    .mapValues((results, duration) => [duration, results.length])
-    .values()
-    .value();
-}
-
-async function run() {
-  const result = await runBenchmarks(10000);
+async function showGraph(times) {
+  const result = await runBenchmarks(times);
   const graphs = [];
 
   result.forEach((suite, suitePath) =>
     suite.forEach((scenario, scenarioName) => {
       graphs.push({
         name: scenarioName,
-        data: prepareDataForGraph(scenario.data),
+        data: scenario.data,
         median: scenario.median,
         mean: scenario.mean,
       });
     }),
   );
 
-  const app = express();
-
-  app.set('view engine', 'ejs');
-
-  app.get('/', (req, res) => {
-    res.render('./chart.ejs', { graphs: JSON.stringify(graphs) });
-  });
-
-  app.listen(4000, () => {
-    openBrowser('http://localhost:4000');
-  });
+  renderGraphs(graphs);
 }
 
-run();
+async function showGraphMultipleBenchmarks(times = 5) {
+  const graphs = [];
+  const results = [];
+
+  for (let i = 0; i < times; i++) {
+    results.push(await runBenchmarks(10000));
+  }
+
+  results.forEach((result, index) => {
+    const suite = result.get(
+      '/Users/rany/Projects/performance-degradation-testing/__benchmarks__/fibonacci.js',
+    );
+
+    const scenarioName = 'recursiveWithMemoization';
+    const scenario = suite.get(scenarioName);
+
+    graphs.push({
+      name: scenarioName + index,
+      data: scenario.data,
+      median: scenario.median,
+      mean: scenario.mean,
+    });
+  });
+
+  renderGraphs(graphs);
+}
+
+showGraph(100000);
+// showGraphMultipleBenchmarks(20);
